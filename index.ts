f1a2be7857bf8b2de1c4c5217147b1a5a1dae3fb
khaimov <haimovroman12@yandex.ru>
2018-12-09 17:32:54 +0300

    Create first code from AST

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log('Hello TypeScript!');
+import * as escodegen from 'escodegen';
+
+const code = escodegen.generate({
+  type: 'BinaryExpression',
+  operator: '+',
+  left: { type: 'Literal', value: 40 },
+  right: { type: 'Literal', value: 2 },
+});
+
+console.log(code);
